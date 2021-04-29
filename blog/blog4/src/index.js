df326f74862f320d22d14b267873903d4a38af1c
liuyuchen <liuyuchen.hust@gmail.com>
2021-04-29 04:27:21 +0000

    add 4.29

---
+++ blog/blog4/src/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: Liu Yuchen
+ * @Date: 2021-04-29 00:47:07
+ * @LastEditors: Liu Yuchen
+ * @LastEditTime: 2021-04-29 03:52:26
+ * @Description: 
+ * @FilePath: /liuyuchen777.github.io/blog/blog4/src/index.js
+ * @GitHub: https://github.com/liuyuchen777
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
